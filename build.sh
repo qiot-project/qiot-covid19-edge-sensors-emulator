6127ac68cdc7ac725c860c233ba49baeab39bef2
abattagl <abattagl@localhost>
2021-01-19 14:55:46 +0100

    Switch container image repo Update Quarkus version Rename root package

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-./mvnw clean package -Pnative  -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/qiot-sensors-emulator:1.0.1-x86_64 --force
-docker  build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-sensors-emulator:1.0.1-x86_64 .
-docker push quay.io/qiotcovid19/qiot-sensors-emulator:1.0.1-x86_64
-#sudo podman run -it --rm -p 5000:5000 --net qiot-covid19-edge quay.io/qiotcovid19/qiot-sensors-emulator:1-x86_64
-#curl localhost:5000/gas/all
+chmod +x mvnw
+./mvnw clean package -Pnative  -Dquarkus.profile=staging -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
+docker rmi quay.io/qiotcovid19/edge-integrator:1.0.1-x86_64 --force
+docker build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/edge-integrator:1.0.1-x86_64 .
+docker push quay.io/qiotcovid19/edge-integrator:1.0.1-x86_64
+#docker run -it --rm -p 8080:8080 --net host quay.io/qiot/qiot-integrator
---
+++ buildaarch64.sh
@@ -0,0 +1,4 @@
+docker rmi quay.io/qiotcovid19/edge-sensors-emulator:1.0.1-aarch64 --force
+docker build -f src/main/docker/Dockerfile.native.multiarch -t quay.io/qiotcovid19/edge-sensors-emulator:1.0.1-aarch64 .
+docker push quay.io/qiotcovid19/edge-sensors-emulator:1.0.1-aarch64
+#docker run -it --rm -p 8080:8080 --net host quay.io/qiot/qiot-integrator
