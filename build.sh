75f932e8f68a2edb780a82d55be471ca265fd3db
abattagl <abattagl@localhost>
2021-01-19 10:40:48 +0100

    build script rootless upgraded quarkus properties

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./mvnw clean package -Pnative  -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-sudo podman rmi quay.io/qiotcovid19/qiot-sensors-emulator:1-x86_64 --force
-sudo podman  build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-sensors-emulator:1-x86_64 .
-sudo podman push quay.io/qiotcovid19/qiot-sensors-emulator:1-x86_64
+docker rmi quay.io/qiotcovid19/qiot-sensors-emulator:1.0.1-x86_64 --force
+docker  build -f src/main/docker/Dockerfile.native -t quay.io/qiotcovid19/qiot-sensors-emulator:1.0.1-x86_64 .
+docker push quay.io/qiotcovid19/qiot-sensors-emulator:1.0.1-x86_64
 #sudo podman run -it --rm -p 5000:5000 --net qiot-covid19-edge quay.io/qiotcovid19/qiot-sensors-emulator:1-x86_64
 #curl localhost:5000/gas/all
