8987507e9f83a9551582a8aa1053320c6a4beb43
abattagl <abattagl@localhost>
2021-01-21 17:21:41 +0100

    build script fix removed system id rest api

--- buildaarch64.sh
+++ buildaarch64.sh
@@ -1,3 +1,4 @@
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker rmi quay.io/qiotcovid19/edge-sensors-emulator:1.0.1-aarch64 --force
 docker build -f src/main/docker/Dockerfile.native.multiarch -t quay.io/qiotcovid19/edge-sensors-emulator:1.0.1-aarch64 .
 docker push quay.io/qiotcovid19/edge-sensors-emulator:1.0.1-aarch64
